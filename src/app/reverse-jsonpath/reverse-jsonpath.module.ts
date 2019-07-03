281a4e029b4a00fc50f011cff52db054be77b794
Himanshu Tamrakar <himanshu.tamrakar@decimal.co.in>
2019-07-03 11:54:19 +0530

    reverse json module add

---
+++ src/app/reverse-jsonpath/reverse-jsonpath.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { ReverseJsonpathRoutingModule } from './reverse-jsonpath-routing.module';
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    ReverseJsonpathRoutingModule
+  ]
+})
+export class ReverseJsonpathModule { }
