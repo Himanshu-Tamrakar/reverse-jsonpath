ff12b04a31509011b4372a706eb18bbd5b4f3d64
Himanshu Tamrakar <himanshu.tamrakar@decimal.co.in>
2019-07-03 14:42:46 +0530

    shared services added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { CommonService } from './shared/services/common.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'reverse-jsonpath';
+
+  constructor(private commonService:CommonService) {
+      this.commonService.test();
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { StaticModule } from './shared/services/static.module';
 
 @NgModule({
   declarations: [
@@ -10,7 +11,8 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    StaticModule.forRoot(),
   ],
   providers: [],
   bootstrap: [AppComponent]
